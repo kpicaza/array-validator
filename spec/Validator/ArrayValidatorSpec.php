47a2c13075d844697c1055003c0998c5387bd81a
kpicaza <kpicaza.sound@gmail.com>
2018-09-20 19:09:49 +0200

    start adding test cases

--- spec/Validator/ArrayValidatorSpec.php
+++ spec/Validator/ArrayValidatorSpec.php
@@ -8,9 +8,41 @@ use Prophecy\Argument;
 
 class ArrayValidatorSpec extends ObjectBehavior
 {
-    const VALUES_1 = [
-
+    const VALID_VALUES_1 = [
+        'user_id' => 'SomeId',
+        'email' => 'example@example.com',
+        'name' => 'Mr Potato',
+        'descriptioarn' => 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
     ];
+
+    const INVALID_VALUES_1 = [
+        'user_id' => '',
+        'email' => 'example@example.com',
+        'name' => 'Mr Potato',
+        'descriptioarn' => 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    ];
+
+    const INVALID_VALUES_1_2 = [
+        'user_id' => 'someId',
+        'email' => 'example.example.com',
+        'name' => 'Mr Potato',
+        'description' => 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    ];
+
+    const INVALID_VALUES_1_3 = [
+        'user_id' => 'someId',
+        'email' => 'example@example.com',
+        'name' => 'Mr',
+        'description' => 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    ];
+
+    const INVALID_VALUES_1_4 = [
+        'user_id' => 'someId',
+        'email' => 'example@example.com',
+        'name' => 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+        'description' => 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+    ];
+
     const RULES_1 = [
         'user_id' => 'notEmpty',
         'email' => 'notEmpty|email',
@@ -18,8 +50,39 @@ class ArrayValidatorSpec extends ObjectBehavior
         'description' => 'notEmpty|greaterThan:40'
     ];
 
-    function it_is_initializable()
+    function it_should_do_nothing_with_valid_parameters()
     {
-        $this->shouldHaveType(ArrayValidator::class);
+        $this->beConstructedThrough('check', [
+           self::VALID_VALUES_1,
+           self::RULES_1
+        ]);
+    }
+
+    function it_should_thrown_an_exception_when_user_id_is_empty()
+    {
+        $this->shouldThrow(
+            \Throwable::class
+        )->during('check', self::INVALID_VALUES_1, self::RULES_1);
+    }
+
+    function it_should_thrown_an_exception_when_email_is_not_well_formed()
+    {
+        $this->shouldThrow(
+            \Throwable::class
+        )->during('check', self::INVALID_VALUES_1_2, self::RULES_1);
+    }
+
+    function it_should_thrown_an_exception_when_name_is_less_than_2()
+    {
+        $this->shouldThrow(
+            \Throwable::class
+        )->during('check', self::INVALID_VALUES_1_3, self::RULES_1);
+    }
+
+    function it_should_thrown_an_exception_when_name_is_greater_than_120()
+    {
+        $this->shouldThrow(
+            \Throwable::class
+        )->during('check', self::INVALID_VALUES_1_4, self::RULES_1);
     }
 }
